8076df301271df091db9aac02ce500dde2c982f7
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-12-13 19:58:50 -0500

    Move helloTest.js and get it to fail to test mocha

---
+++ test/helloTest.js
@@ -0,0 +1,18 @@
+console.log("Hello world test under development");
+
+var expect = require("chai").expect;
+
+describe("Color Code Converter", function() {
+  describe("RGB to Hex conversion", function() {
+    it("converts the basic colors", function() {
+        var result = 10;
+        expect(result).to.equal("ff0000");
+    });
+  });
+
+  describe("Hex to RGB conversion", function() {
+    it("converts the basic colors", function() {
+
+    });
+  });
+});
--- test/server-tests/helloTest.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello world test under development");
