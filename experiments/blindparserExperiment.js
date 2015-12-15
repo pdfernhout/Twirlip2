5f3d9554318e5a359e2bf5077e80651936f86122
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-12-15 15:34:30 -0500

    Ass FSF RSS feed example test data and URL

--- experiments/blindparserExperiment.js
+++ experiments/blindparserExperiment.js
@@ -1,7 +1,8 @@
 var parser = require('blindparser');
  
 // var url = 'http://rss.cnn.com/rss/cnn_topstories.rss';
-var url = 'http://portland.craigslist.org/sof/index.rss';
+// var url = 'http://portland.craigslist.org/sof/index.rss';
+var url = 'http://static.fsf.org/fsforg/rss/news.xml';
 
 // with no options
 parser.parseURL(url, function(err, out){
