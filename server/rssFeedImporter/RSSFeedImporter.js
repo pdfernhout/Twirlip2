d3c605c526786fcc6ed6582e92f9700038884891
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-12-16 13:00:35 -0500

    WIP on RSSFeedImporter.js

---
+++ server/rssFeedImporter/RSSFeedImporter.js
@@ -0,0 +1,22 @@
+var parser = require('blindparser');
+ 
+// var url = 'http://rss.cnn.com/rss/cnn_topstories.rss';
+// var url = 'http://portland.craigslist.org/sof/index.rss';
+// var url = 'http://static.fsf.org/fsforg/rss/news.xml';
+
+function RSSFeedImporter(store) {
+	this.store = store;
+}
+
+RSSFeedImporter.prototype.readFeed = function(url) {
+	// with no options
+	parser.parseURL(url, function(err, out) {
+	    console.log(JSON.stringify(out, null, 2));
+	});
+};
+
+RSSFeedImporter.prototype.storeFeed = function(data) {
+	
+};
+
+module.exports = RSSFeedImporter;
