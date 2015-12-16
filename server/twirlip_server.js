3d7ff2da99e53ee32924ee4cc3be330067b53549
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-12-15 22:45:14 -0500

    Display initial webapp page withplease wait loading animation
    This code is based on NarraFirma's approach.

--- server/twirlip_server.js
+++ server/twirlip_server.js
@@ -1,10 +1,73 @@
-var http = require('http');
+// Test at: http://localhost:9000
+/*jslint node: true */
+"use strict";
 
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("ThunderbirdS Are Grow!\n");
+// Standard nodejs modules
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var util = require('util');
+
+// The modules below require npm installation
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Main code
+
+function applicationLog() {
+    var newArguments = [new Date().toISOString()].concat(Array.prototype.slice.call(arguments));
+    console.log.apply(console, newArguments);
+}
+
+applicationLog("================== Twirlip server for Node.js started ================");
+
+applicationLog("__dirname", __dirname);
+
+var app = express();
+
+var logger = function(request, response, next) {
+    applicationLog("Request:", request.method, request.url);
+    next();
+};
+
+app.use(logger);
+
+// TODO: Could there be an issue with bodyParser with undeleted temp files? (Mentioned somewhere online)
+// includes support to parse JSON-encoded bodies (and saving the rawBody)
+app.use(bodyParser.json({
+    limit: '10mb'
+}));
+
+// to support URL-encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Application routes
+
+app.use("/$", function(req, res) {
+    res.redirect('/twirlip.html');
 });
 
-server.listen(9000, "127.0.0.1");
+app.use("/", express.static(__dirname + "/../webapp"));
 
-console.log("Server running at http://127.0.0.1:9000/");
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.use("/", function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("ThunderbirdS Are Grow!\n");
+});
+
+var server = http.createServer(app);
+
+server.listen(9000, "127.0.0.1", function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    applicationLog(util.format("Twirlip server listening at http://%s:%s", host, port));
+});
+
+console.log("Twirlip server running at http://127.0.0.1:9000/");
