11de346a2ef4e86502c88df074cb9ba1e6a0022a
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-12-17 13:16:42 -0500

    Improve display of test RSS data

--- webapp/source/plugins-for-local-libre-or-standard-services/rss/rss-plugin.ts
+++ webapp/source/plugins-for-local-libre-or-standard-services/rss/rss-plugin.ts
@@ -7,6 +7,34 @@ export function initialize() {
     console.log("RSS plugin initialized");
 }
 
+function displayItem(item) {
+    var title = item.title;
+    var link = item.link;
+    var description = item.desc;
+    var timestamp = item.date;
+
+    return m("div.item", [
+        m("br")
+        m("strong", title),
+        m("br")
+        description,
+        " ",
+        m("a", { href: link }, "More ..."),
+        m("br"),
+    ]);
+}
+
+function displayRSS() {
+    return m("div.feed", [
+        testData.items.map(displayItem);
+	]);
+}
+
 export function display() {
-    return m("div.rssPlugin", ["RSS feed reader plugin", m("pre", JSON.stringify(testData, null, 2))]);
+    return m("div.rssPlugin", [
+        m("hr"),
+        m("strong", "RSS feed reader plugin"),
+        //m("pre", JSON.stringify(testData, null, 2))
+        displayRSS()
+    ]);
 }
