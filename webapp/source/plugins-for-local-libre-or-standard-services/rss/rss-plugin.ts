3495287f9571922ca6d2942088eaf1b3bc126207
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-12-17 12:40:01 -0500

    Improve rss stub to display JSON from a preloaded test RSS feed.

--- webapp/source/plugins-for-local-libre-or-standard-services/rss/rss-plugin.ts
+++ webapp/source/plugins-for-local-libre-or-standard-services/rss/rss-plugin.ts
@@ -1,9 +1,12 @@
 import m = require("mithril");
 
+import testData = require("./rssFeedExampleFromFSF");
+
 export function initialize() {
+    console.log("test data:", testData)
     console.log("RSS plugin initialized");
 }
 
 export function display() {
-    return m("div.rssPlugin", ["RSS feed reader plugin"]);
+    return m("div.rssPlugin", ["RSS feed reader plugin", m("pre", JSON.stringify(testData, null, 2))]);
 }
---
+++ webapp/source/plugins-for-local-libre-or-standard-services/rss/rssFeedExampleFromFSF.ts
@@ -0,0 +1,205 @@
+// Used for initial development testing
+	
+export = {
+    "type": "rss",
+    "metadata": {
+        "title": [
+            "FSF News"
+        ],
+        "desc": [
+            ""
+        ],
+        "url": [
+            "http://www.fsf.org/news/aggregator"
+        ],
+        "image": [
+            {
+                "rdf:resource": [
+                    "http://www.fsf.org/logo.png"
+                ]
+            }
+        ]
+    },
+    "items": [
+        {
+            "title": [
+                "Libreboot T400 laptop now FSF-certified to respect your freedom"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Friday, December 11, 2015 -- The Free Software Foundation (FSF) today awarded Respects Your Freedom (RYF) certification to the Libreboot T400 laptop as sold by Minifree. The RYF certification mark means that the product meets the FSF's standards in regard to users' freedom, control over the product, and privacy."
+            ],
+            "link": [
+                "http://www.fsf.org/news/libreboot-t400-laptop-now-fsf-certified-to-respect-your-freedom"
+            ],
+            "date": 1449867945000
+        },
+        {
+            "title": [
+                "Library of Congress issues limited exemptions to DMCA anti-circumvention provisions but leaves users without full control over their own computing"
+            ],
+            "desc": [
+                "On October 27th, 2015, the Library of Congress published its list of exemptions to the Digital Millennium Copyright Act (DMCA), denying many of the proposed exemptions and modifying or limiting others. Because of this, users of a wide variety of software and devices, such as ebook readers and video game consoles, still do not have the right to control their own computing. Even for the areas where the exemptions permit users some additional control, they do not permit sharing of methods for obtaining it, and last only three years."
+            ],
+            "link": [
+                "http://www.fsf.org/news/library-of-congress-issues-limited-exemptions-to-dmca-anti-circumvention-provisions-but-leaves-users-without-full-control-over-their-own-computing"
+            ],
+            "date": 1446239700000
+        },
+        {
+            "title": [
+                "GNU ethical criteria for code repositories emphasize privacy, freedom, and copyleft"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Friday, October 16th, 2015 -- Today the Free Software Foundation and the GNU Project announced the first version of criteria for evaluating services that host free software source code repositories for distribution and collaborative development. Developed with the leadership of Richard Stallman and GNU volunteers, the criteria provide a framework for code repositories to ensure that they respect their users in a manner consonant with the values of the free software movement, and for users to hold these crucial institutions accountable."
+            ],
+            "link": [
+                "http://www.fsf.org/news/gnu-ethical-repo-criteria"
+            ],
+            "date": 1445018286000
+        },
+        {
+            "title": [
+                "FSF, Conservancy publish principles for community-oriented GPL enforcement"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Thursday, October 1st, 2015 -- The Free Software Foundation (FSF) today announced publication of \"The Principles of Community-Oriented GPL Enforcement,\" co-authored with the Software Freedom Conservancy. The document lays out the principles that both organizations follow when they receive reports that a company is violating copyleft terms like the GNU General Public License (GNU GPL)."
+            ],
+            "link": [
+                "http://www.fsf.org/news/fsf-conservancy-publish-principles-for-community-oriented-gpl-enforcement"
+            ],
+            "date": 1443716400000
+        },
+        {
+            "title": [
+                "Taurinus X200 laptop now FSF-certified to respect your freedom"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Monday, September 28, 2015 -- The Free Software Foundation (FSF) today awarded Respects Your Freedom (RYF) certification to the Taurinus X200 laptop sold by Libiquity LLC. The RYF certification mark means that the product meets the FSF's standards in regard to users' freedom, control over the product, and privacy."
+            ],
+            "link": [
+                "http://www.fsf.org/news/now-fsf-cerified-to-respect-your-freedom-taurinus-x200"
+            ],
+            "date": 1443476400000
+        },
+        {
+            "title": [
+                "Free Software Foundation seeks nominations for 18th annual Free Software Awards"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Thursday, September 10, 2015 -- The Free Software Foundation (FSF) and the GNU Project today call upon the free software community to submit nominations for the 18th annual Free Software Awards. The Free Software Awards include the Award for the Advancement of Free Software and the Award for Projects of Social Benefit. The awards are presented each year at the LibrePlanet free software conference, and at the same time nominations for the next year's awards open."
+            ],
+            "link": [
+                "http://www.fsf.org/news/free-software-foundation-seeks-nominations-for-18th-annual-free-software-awards"
+            ],
+            "date": 1441904100000
+        },
+        {
+            "title": [
+                "The FSF's statement on Windows 10"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Thursday, July 30, 2015 -- The Free Software Foundation urges everyone to reject Windows 10 and join us in the world of free software. Like all proprietary software, Windows 10 puts those that use it under the thumb of its owner. Free software like the GNU/Linux operating system treats users as equals and gives them control over their digital lives."
+            ],
+            "link": [
+                "http://www.fsf.org/news/the-fsfs-statement-on-windows-10"
+            ],
+            "date": 1438292100000
+        },
+        {
+            "title": [
+                "FSF invites the free software community to its 30th birthday party on October 3rd, 2015"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Friday, July 24, 2015 -- Today the Free Software Foundation (FSF) announced an open invitation to its thirtieth birthday party, to be held in Boston on the evening of Saturday, October 3rd. The party will feature an address by FSF founder and president Richard Stallman. It represents the culmination of an outstanding thirtieth anniversary year, including the biggest LibrePlanet conference ever and an article about GNU in the New Yorker."
+            ],
+            "link": [
+                "http://www.fsf.org/news/fsf-invites-the-free-software-community-to-its-30th-birthday-party-on-october-3rd-2015"
+            ],
+            "date": 1437752400000
+        },
+        {
+            "title": [
+                "Statement on Canonical's updated licensing terms for Ubuntu GNU/Linux"
+            ],
+            "desc": [
+                "On July 15th, 2015, the Free Software Foundation's Licensing and Compliance Lab, along with the Software Freedom Conservancy, announces that, after two years of negotiations, Canonical, Ltd. has published an update to the licensing terms of Ubuntu GNU/Linux."
+            ],
+            "link": [
+                "http://www.fsf.org/news/canonical-updated-licensing-terms"
+            ],
+            "date": 1436970000000
+        },
+        {
+            "title": [
+                "FSF endorses embedded GNU/Linux distro ProteanOS as fully free"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Wednesday, July 8, 2015 -- The Free Software Foundation (FSF) today announced the addition of ProteanOS to its list of recommended GNU/Linux distributions."
+            ],
+            "link": [
+                "http://www.fsf.org/news/fsf-endorses-embedded-gnu-linux-distro-proteanos-as-fully-free"
+            ],
+            "date": 1436383500000
+        },
+        {
+            "title": [
+                "The FSF is hiring: Seeking a full-time outreach and communication coordinator"
+            ],
+            "desc": [
+                "Our current outreach and communication coordinator has moved into a new position at the FSF, so we are looking for another person to fill the role."
+            ],
+            "link": [
+                "http://www.fsf.org/news/the-fsf-is-hiring-a-full-time-outreach-and-communication-coordinator"
+            ],
+            "date": 1434479100000
+        },
+        {
+            "title": [
+                "Free Software Foundation announces deputy director search"
+            ],
+            "desc": [
+                "The Free Software Foundation (FSF), a Boston-based 501(c)(3) charity with a worldwide mission to protect freedoms critical to the computer-using public, would love to find an experienced, Boston-based deputy director to expand our leadership team."
+            ],
+            "link": [
+                "http://www.fsf.org/news/free-software-foundation-announces-deputy-director-search"
+            ],
+            "date": 1431546900000
+        },
+        {
+            "title": [
+                "The FSF is hiring: Seeking a Boston-area full-time web developer"
+            ],
+            "desc": [
+                "The Free Software Foundation (FSF), a Boston-based 501(c)(3) charity with a worldwide mission to protect freedoms critical to the computer-using public, seeks a Boston-based individual to be its full-time web developer."
+            ],
+            "link": [
+                "http://www.fsf.org/news/the-fsf-is-hiring-seeking-a-full-time-web-developer"
+            ],
+            "date": 1431026700000
+        },
+        {
+            "title": [
+                "Community is the focus of 2015's International Day Against DRM"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Wednesday, May 6th, 2015 -- Today, a wide variety of community groups, activist organizations, and businesses are taking part in the ninth International Day Against DRM (dayagainstdrm.org)."
+            ],
+            "link": [
+                "http://www.fsf.org/news/community-is-the-focus-of-2015s-international-day-against-drm"
+            ],
+            "date": 1430862000000
+        },
+        {
+            "title": [
+                "LibrePlanet 2015 brings free software luminaries to MIT"
+            ],
+            "desc": [
+                "BOSTON, Massachusetts, USA -- Monday, March 23, 2015 -- The 2015 LibrePlanet free software conference drew nearly 350 activists from around the world to discuss issues of freedom, privacy, and security in computing. Free Software Foundation founder and president Richard Stallman delivered the opening keynote, \"Free software, free hardware, and other things\" before a packed room at MIT's Stata Center, with hundreds of remote participants tuning in online."
+            ],
+            "link": [
+                "http://www.fsf.org/news/libreplanet-2015-brings-free-software-luminaries-to-mit"
+            ],
+            "date": 1427233200000
+        }
+    ]
+}
