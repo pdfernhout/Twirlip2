03e7c757091975ddf540273a1d294f98755b091b
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-12-22 06:40:51 -0500

    Update StoreMemory and test for new Pointrel specifications

--- server/pointrel20151212/StoreMemory.js
+++ server/pointrel20151212/StoreMemory.js
@@ -5,6 +5,11 @@ var crypto = require('crypto');
 var uuid = require('node-uuid');
 
 var maxSkeinLength = 4096000;
+// TODO: Use this in storeDataString for less memory allocation: var sharedOutputBuffer = new Buffer(maxSkeinLength);
+var itemPrefix = "@@@PCE ";
+
+// 64 is assumed maximum length for now of possible lengths of length strings combined like for PCE@@@ 999999999999999 999999999999999
+var maximumLengthOfAreaIncludingPrefixAndLengths = 64;
 
 var debugLogging = false;
 
@@ -19,7 +24,7 @@ function makeAlwaysSuccessfulPromise(result) {
 
 function makeSkeinName() {
 	var uuid4 = uuid.v4();
-    return "skein-" + uuid4 + ".twirlip";
+    return "skein-" + uuid4 + ".pces";
 }
 
 function StoreMemory(defaultMetadata) {
@@ -33,16 +38,23 @@ function StoreMemory(defaultMetadata) {
 StoreMemory.prototype.allocateNewSkein = function() {
     this.currentSkeinName = makeSkeinName();
     if (debugLogging) console.log("StoreMemory: Making new skein", this.currentSkeinName);
-    this.skeins[this.currentSkeinName] = "";
+    this.skeins[this.currentSkeinName] = {buffer: new Buffer(maxSkeinLength), length: 0};
 };
 
-StoreMemory.prototype.writeToCurrentSkein = function(dataString) {
-	if (this.skeins[this.currentSkeinName].length > maxSkeinLength) {
+// TODO: Support automatically putting big objects in another skein to optimize writing performance by reducing latency for small objects
+StoreMemory.prototype.writeToCurrentSkein = function(buffer) {
+	if (this.skeins[this.currentSkeinName].length + buffer.length > maxSkeinLength) {
 		this.allocateNewSkein();
 	}
-	var start = this.skeins[this.currentSkeinName].length + 1;
-	this.skeins[this.currentSkeinName] += "\n" + dataString + "\n";
-	return {skeinName: this.currentSkeinName, start: start, length: dataString.length};
+	var skein = this.skeins[this.currentSkeinName];
+	var start = skein.length;
+	var writtenCount = buffer.copy(skein.buffer, start, 0);
+	if (debugLogging) console.log("writtenCount", writtenCount);
+	if (writtenCount !== buffer.length) {
+	    throw new Error("skein.write did not provide expected value: " + writtenCount + " expected: " + buffer.length);
+	}
+	skein.length += writtenCount;
+	return {skeinName: this.currentSkeinName, start: start, length: buffer.length};
 };
 
 StoreMemory.prototype.wrap = function(what, metadata) {      
@@ -60,33 +72,169 @@ StoreMemory.prototype.wrap = function(what, metadata) {
     return wrapper;
 };
 
-StoreMemory.prototype.store = function(data) {
-    var dataAsString = JSON.stringify(data);
-    if (dataAsString.length > maxSkeinLength) throw new Error("data to be stored is too long: " + dataAsString.length);
-    var sha256 = crypto.createHash('sha256').update(dataAsString, 'utf8').digest('hex');
+function isString(value) {
+    return typeof value === 'string' || value instanceof String;
+}
+
+function stringStartsWith(string, prefix) {
+    return string.slice(0, prefix.length) === prefix;
+}
+
+StoreMemory.prototype.storeDataString = function(dataAsString, extraMetadata, skipDuplicates) {
+    // TODO: Should probably copy metadata
+    // TODO: Should add default metadata
     
-	var location = this.sha256ToSkeinAndPosition[sha256];
-	if (!location) {
-	    location = this.writeToCurrentSkein(dataAsString);
-	    if (debugLogging) console.log("store: sha256 and location of data to store", sha256, location, dataAsString.substring(0, 40) + "...");
-	    this.sha256ToSkeinAndPosition[sha256] = location;
-	} else {
-		if (debugLogging) console.log("store: previously stored ", sha256, location, dataAsString.substring(0, 40) + "...");
-	}
-	
+    // Quick sanity check
+    if (dataAsString.length > maxSkeinLength) throw new Error("dataAsString to be stored is too long: " + dataAsString.length);
+    
+    if (!isString(dataAsString)) {
+        console.log("StoreMemory: dataString is not a string", dataAsString);
+        throw new Error("dataString is not a string");
+    }
+    
+    var dataBuffer = new Buffer(dataAsString, "utf8");
+    var sha256 = crypto.createHash('sha256').update(dataBuffer).digest('hex');
+    
+    var metadata = JSON.parse(JSON.stringify(this.defaultMetadata));
+    for (var key in extraMetadata) {
+        if (extraMetadata.hasOwnProperty(key)) {
+            metadata[key] = extraMetadata[key];
+        }
+    }
+    
+    metadata.sha256 = sha256;
+    metadata.length = dataBuffer.length;
+    metadata.timestamp = metadata.timestamp || new Date().toISOString();
+    
+    var metadataAsString = JSON.stringify(metadata);
+    var metadataBuffer = new Buffer(metadataAsString, "utf8");
+    
+    // console.log("metadataAsString", metadataAsString);
+    // console.log("metadataBuffer.length", metadataBuffer.length);
+    
+    // TODO: Think about whether to include SHA256 with length
+    var start = itemPrefix + metadataBuffer.length + " " + dataBuffer.length + " ";
+    var startBuffer = new Buffer(start, "utf8");
+    
+    var newLineBuffer = new Buffer("\n", "utf8");
+    
+    var totalLength = newLineBuffer.length + startBuffer.length + metadataBuffer.length + newLineBuffer.length + dataBuffer.length + newLineBuffer.length;          
+        
+    if (totalLength > maxSkeinLength) throw new Error("prepared data to be stored is too long: " + dataBuffer.length + " " + metadataBuffer.length);
+    
+    var outputBuffer = Buffer.concat([newLineBuffer, startBuffer, metadataBuffer, newLineBuffer, dataBuffer, newLineBuffer]);
+    
+    var location = this.sha256ToSkeinAndPosition[sha256];
+    
+    if (skipDuplicates && location) {
+        if (debugLogging) console.log("store: skipDuplicates and previously stored ", sha256, location, dataAsString.substring(0, 40) + "...");
+    } else {
+        var newLocation = this.writeToCurrentSkein(outputBuffer);
+        if (debugLogging) console.log("store: sha256 and location of data to store", sha256, newLocation, dataAsString.substring(0, 40) + "...");
+        
+        // Store a position if only one, or an array of them
+        if (location === undefined) {
+            location = newLocation;
+        } else if (Array.isArray(location)) {
+            location.push(newLocation);
+        } else {
+            location = [location, newLocation];
+        }
+        this.sha256ToSkeinAndPosition[sha256] = location;
+    }
+    
+    if (debugLogging) console.log("storeDataString sha256", sha256);
     return makeAlwaysSuccessfulPromise(sha256);
 };
 
-StoreMemory.prototype.fetch = function(sha256) {
+StoreMemory.prototype.storeDataObject = function(data, extraMetadata, skipDuplicates) {
+    var dataAsString = JSON.stringify(data, null, 2);
+    if (!extraMetadata) extraMetadata = {};
+    extraMetadata.format = "application/json";
+    return this.storeDataString(dataAsString, extraMetadata, skipDuplicates);
+};
+
+StoreMemory.prototype.fetchDataString = function(sha256, options) {
+    // console.log("locations", JSON.stringify(this.sha256ToSkeinAndPosition, null, 2));
+    // console.log("fetchDataString", sha256);
 	var location = this.sha256ToSkeinAndPosition[sha256];
-	if (!location) return null;
+	
+	if (!location) return makeAlwaysSuccessfulPromise(null);
+	
+	// If more than one, use the last one added or loaded
+	if (Array.isArray(location)) location = location[location.length - 1];
+	
 	// console.log("location", location);
-	// console.log("skein", this.skeins[location.skeinName]);
-    var resultString = this.skeins[location.skeinName].substring(location.start, location.start + location.length);
-    // console.log("retrieved resultString", "|" + resultString + "|");
-    var result = JSON.parse(resultString);
+	
+	var skein = this.skeins[location.skeinName];
+	// console.log("skein", skein);
+	
+	var buffer = skein.buffer;
+	
+	// Check start header
+	var start = buffer.toString("utf8", location.start, location.start + maximumLengthOfAreaIncludingPrefixAndLengths);
+	
+	if (!stringStartsWith(start, "\n")) throw new Error("stored item does not start with newline: " + JSON.stringify(location));
+	
+    if (!stringStartsWith(start, "\n" + itemPrefix)) throw new Error("stored item does not start with correct prefix: " + JSON.stringify(location));
+
+    // Read lengths
+    
+    var lengthString = start.substring(1 + itemPrefix.length);
+    
+    // console.log("lengthString", lengthString);
+    
+    var metadataLengthStringLength = lengthString.indexOf(" ");
+    if (metadataLengthStringLength === -1) throw new Error("incorrect metadata length definition: " + lengthString);
+    var metadataLengthString = lengthString.substring(0, metadataLengthStringLength);
+    // console.log("metadataLengthString", metadataLengthString);
+    var metadataLength = parseInt(metadataLengthString);
+    if (isNaN(metadataLength) || metadataLength < 1) throw new Error("incorrect metadata length definition: " + lengthString);
+    // console.log("metadataLength", metadataLength);
+    
+    lengthString = lengthString.substring(metadataLengthStringLength + 1);
+    
+    var dataLengthStringLength = lengthString.indexOf(" ");
+    if (dataLengthStringLength === -1) throw new Error("incorrect data length definition: " + lengthString);
+    var dataLengthString = lengthString.substring(0, dataLengthStringLength);
+    // console.log("dataLengthString", dataLengthString);
+    var dataLength = parseInt(dataLengthString);
+    if (isNaN(dataLength) || dataLength < 1) throw new Error("incorrect data length definition: " + lengthString);
+    // console.log("dataLength", dataLength);
+    
+	// Read metadata
+    var metadataStart = location.start + 1 + itemPrefix.length + metadataLengthStringLength + 1 + dataLengthStringLength + 1;
+    var metadataString = buffer.toString("utf8", metadataStart, metadataStart + metadataLength);
+    // console.log("metadataString: " + metadataString + "|");
+    // console.log("metadataString.length", metadataString.length);
+    var metadata = JSON.parse(metadataString);
+    // console.log("metadata", metadata);
+    
+	// Read data
+    var dataStart = metadataStart + metadataLength + 1;
+    var dataString = buffer.toString("utf8", dataStart, dataStart + dataLength);
+    // console.log("dataString: " + dataString + "|");
+    
+	// Confirm last newline
+    var newlineAtEndStart = dataStart + dataLength;
+    var newlineAtEnd = buffer.toString("utf8", newlineAtEndStart, newlineAtEndStart + 1);
+    if (newlineAtEnd !== "\n") throw new Error("unexpected character for final newline: " + newlineAtEnd);
+	
+    var result = dataString;
+    // TODO: Maybe flag name should be contentType and not format?
+    if (options.parseJSON && !metadata.format || metadata.format === "application/json") result = JSON.parse(dataString);
+    
+    if (!options.returnOnlyData) result = {
+        metadata: metadata,
+        data: result
+    };
+    
     // console.log("retrieved result", result);
     return makeAlwaysSuccessfulPromise(result);
+};
+
+StoreMemory.prototype.fetchDataObject = function(sha256) {
+    return this.fetchDataString(sha256, {returnOnlyData: true, parseJSON: true});
 };
     
 module.exports = StoreMemory;
--- test/StoreMemoryTest.js
+++ test/StoreMemoryTest.js
@@ -11,11 +11,11 @@ describe("Pointrel20151212 StoreMemory test", function() {
     var sha256OfTestString = "4d967a30111bf29f0eba01c448b375c1629b2fed01cdfcc3aed91f1b57d5dd5e";
     
     var testObject = JSON.parse(fs.readFileSync("./test-data/rssFeedExampleFromFSF.json").toString());
-    var sha256OfTestObject = "224ccc878e7b11bca48351b84e2f2a451bc2df3e38bc0eb287d9f942ea4b58a9";
-    var sha256OfWrappedTestObject = "a144d8b7218940a191f3d9d43c8d3ad0784bd7a8c7ba73fa6be8b1efd6f36e0a";
+    var sha256OfTestObject = "608d82ba5d26179308af03671871bd8dc48f00c69f5489b0e162456e1be53b39";
+    var sha256OfWrappedTestObject = "74c03021a8561e4f0eae1a1457a057b1f9df97ed8816fb1807a606a3e2ac5ab1";
     
     it("stores a test string", function() {
-        var promise = store.store(testString);
+        var promise = store.storeDataObject(testString);
         return promise.then(function (result) {
             // console.log("sha256 of store test is", result);
             expect(result).to.equal(sha256OfTestString);
@@ -23,7 +23,7 @@ describe("Pointrel20151212 StoreMemory test", function() {
     });
     
     it("fetches a test string", function() {
-        var promise = store.fetch(sha256OfTestString);
+        var promise = store.fetchDataObject(sha256OfTestString);
         // console.log("about to call promise for fetch", promise);
         return promise.then(function (result) {
             // console.log("result of fetch test is", result);
@@ -32,7 +32,7 @@ describe("Pointrel20151212 StoreMemory test", function() {
     });
     
     it("stores a test object", function() {
-        var promise = store.store(testObject);
+        var promise = store.storeDataObject(testObject);
         return promise.then(function (result) {
             // console.log("sha256 of store object is", result);
             expect(result).to.equal(sha256OfTestObject);
@@ -40,7 +40,15 @@ describe("Pointrel20151212 StoreMemory test", function() {
     });
     
     it("stores a test object a second time", function() {
-        var promise = store.store(testObject);
+        var promise = store.storeDataObject(testObject);
+        return promise.then(function (result) {
+            // console.log("sha256 of store object is", result);
+            expect(result).to.equal(sha256OfTestObject);
+        });
+    });
+    
+    it("stores a test object a third time with skipDuplicates", function() {
+        var promise = store.storeDataObject(testObject, {}, {skipDuplicates: true});
         return promise.then(function (result) {
             // console.log("sha256 of store object is", result);
             expect(result).to.equal(sha256OfTestObject);
@@ -48,7 +56,7 @@ describe("Pointrel20151212 StoreMemory test", function() {
     });
     
     it("stores a wrapped test object", function() {
-        var promise = store.store(store.wrap(testObject, {when: "test"}));
+        var promise = store.storeDataObject(store.wrap(testObject, {when: "test"}));
         return promise.then(function (result) {
             // console.log("sha256 of store object is", result);
             expect(result).to.equal(sha256OfWrappedTestObject);
@@ -56,7 +64,7 @@ describe("Pointrel20151212 StoreMemory test", function() {
     });
     
     it("fetches a test object", function() {
-        var promise = store.fetch(sha256OfTestObject);
+        var promise = store.fetchDataObject(sha256OfTestObject);
         // console.log("about to call promise for fetch", promise);
         return promise.then(function (result) {
             // console.log("result of fetch test is", result);
