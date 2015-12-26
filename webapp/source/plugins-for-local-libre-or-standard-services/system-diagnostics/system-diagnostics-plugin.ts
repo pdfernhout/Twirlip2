7c108405e415e2cb234170fdf9be770c5f8fdf24
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-12-26 11:31:14 -0500

    Improve system diagnostics stub with polling infrastructure

--- webapp/source/plugins-for-local-libre-or-standard-services/system-diagnostics/system-diagnostics-plugin.ts
+++ webapp/source/plugins-for-local-libre-or-standard-services/system-diagnostics/system-diagnostics-plugin.ts
@@ -2,13 +2,44 @@
 
 import m = require("mithril");
 
+var basketNames = [];
+var polling = false;
+var pollingTimeout = null;
+
 export function initialize() {
     console.log("System Diagnostics plugin initialized");
+}
+
+function setPoll(value) {
+    polling = value;
+    
+    if (!polling && pollingTimeout) {
+        clearTimeout(pollingTimeout);
+        pollingTimeout = null;
+        console.log("stop polling for baskets");
+        return;
+    }
+    
+    if (!pollingTimeout) {
+        pollingTimeout = setTimeout(poll, 5000);
+        console.log("start polling for baskets");
+    }
+}
+
+function poll() {
+    console.log("polling");
+    pollingTimeout = setTimeout(poll, 5000);
 }
 
 export function display() {
     return m("div.systemDiagnosticsPlugin", [
         m("hr"),
-        m("strong", "System Diagnostics feed plugin"), m("br")
+        m("strong", "System Diagnostics feed plugin"), m("br"),
+        m("input[type=checkbox]", {onclick: m.withAttr("checked", setPoll), checked: polling}),
+        "Poll for baskets", m("br"),
+        "Baskets: ", m("br"),
+        basketNames.map((basketName) => {
+            return m("div", basketName);
+        })
     ]);
 }
