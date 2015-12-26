ac9eb8b3af312e8e31c0f0d08a865c5786bacd97
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-12-26 12:13:12 -0500

    Improve system diagnostics plugin to show sha256 list and current contents

--- webapp/source/plugins-for-local-libre-or-standard-services/system-diagnostics/system-diagnostics-plugin.ts
+++ webapp/source/plugins-for-local-libre-or-standard-services/system-diagnostics/system-diagnostics-plugin.ts
@@ -3,8 +3,11 @@
 import m = require("mithril");
 
 var basketNames = [];
+var currentBasketName = "";
 var polling = false;
 var pollingTimeout = null;
+var sha256List = [];
+var currentContent = "";
 
 export function initialize() {
     console.log("System Diagnostics plugin initialized");
@@ -30,6 +33,18 @@ function poll() {
     console.log("polling");
     pollingTimeout = null;
     
+    updateBasketList(() => {
+        pollingTimeout = setTimeout(poll, 5000);
+    });
+}
+
+function updateBasketList(callback) {
+    console.log("updateBasketList");
+    
+    sha256List = [];
+    currentBasketName = "";
+    currentContent = "";
+
     m.request({
         method: "POST", 
         url: "/api/store", 
@@ -43,8 +58,60 @@ function poll() {
         } else {
             console.log("polling for basket list failed");
         }
-        pollingTimeout = setTimeout(poll, 5000);
+        if (callback) callback();
     });
+    
+    m.redraw();
+}
+
+function updateSHA256List(basketName, callback) {
+    console.log("updateSHA256List");
+    sha256List = [];
+    currentBasketName = basketName;
+    currentContent = "";
+    
+    m.request({
+        method: "POST", 
+        url: "/api/store", 
+        data: {
+            action: "sha256List",
+            basket: basketName
+        }
+    }).then((result: any) => {
+        console.log("m.request sha256 list result", result);
+        if (result.success) {
+            sha256List = result.sha256List;
+        } else {
+            console.log("polling for sha256 list failed");
+        }
+        if (callback) callback();
+    });
+    
+    m.redraw();
+}
+
+function updateContentsForSHA256(sha256, callback) {
+    currentContent = "";
+    
+    m.request({
+        method: "POST", 
+        url: "/api/store", 
+        data: {
+            action: "fetch",
+            basket: currentBasketName,
+            sha256: sha256
+        }
+    }).then((result: any) => {
+        console.log("m.request sha256 contents result", result);
+        if (result.success) {
+            currentContent = result.content;
+        } else {
+            console.log("polling for sha256 list failed");
+        }
+        if (callback) callback();
+    });
+    
+    m.redraw();
 }
 
 export function display() {
@@ -53,9 +120,17 @@ export function display() {
         m("strong", "System Diagnostics feed plugin"), m("br"),
         m("input[type=checkbox]", {onclick: m.withAttr("checked", setPoll), checked: polling}),
         "Poll for baskets", m("br"),
+        m("button", {onclick: updateBasketList.bind(null, null)}, "Update basket list"), m("br"),
         "Baskets: ", m("br"),
         basketNames.map((basketName) => {
-            return m("div", basketName);
-        })
+            return m("div", m("button", {onclick: updateSHA256List.bind(null, basketName, null)}, basketName));
+        }),
+        "sha256 in basket: ", currentBasketName, m("br"),
+        sha256List.map((sha256) => {
+            return m("div", m("button", {onclick: updateContentsForSHA256.bind(null, sha256, null)}, sha256));
+
+        }),
+        "Current contents:", m("br"),
+        m("pre", JSON.stringify(currentContent, null, 2))
     ]);
 }
